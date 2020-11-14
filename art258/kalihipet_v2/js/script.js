e70bb24e08c2080c861a0a8a93a2043df0032e67
l7ana <59889633+l7ana@users.noreply.github.com>
2020-11-13 17:45:52 -1000

    hh

---
+++ art258/kalihipet_v2/js/script.js
@@ -0,0 +1,25 @@
+// Overlay Nav Menu
+const mymenubutton = document.querySelector('.menu-button');
+const mysitenav = document.querySelector('.site-header .site-nav');
+
+mymenubutton.onclick = function () {
+  if ( mysitenav.getAttribute('data-navstate') ==='open') {
+    mysitenav.setAttribute('data-navstate', 'closed');
+  } else {
+      mysitenav.setAttribute('data-navstate', 'open');
+    }
+  }
+
+// CHANGE ACTIVE STATE FOR ALL SECTIONS WITH INTERSECTION OBSERVER
+    const myobserver = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+          if (entry.isIntersecting) {
+              entry.target.setAttribute('data-sectionstate', 'active');
+          } else {
+              entry.target.setAttribute('data-sectionstate', 'inactive');
+          }
+      });
+  });
+  document.querySelectorAll('.animate-on-scroll').forEach( (section) => {
+      myobserver.observe(section);
+  });

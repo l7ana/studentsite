6dfdd7d21af7c948512ecca49e47108ecf20fe21
l7ana <59889633+l7ana@users.noreply.github.com>
2020-12-11 15:15:56 -1000

    draft 2

--- art258/kalihipet_v2/js/script.js
+++ art258/kalihipet_v2/js/script.js
@@ -1,25 +1,25 @@
 // Overlay Nav Menu
 const mymenubutton = document.querySelector('.menu-button');
-const mysitenav = document.querySelector('.site-header .site-nav');
+const mysitenav = document.querySelector('.site-header .side-nav');
 
 mymenubutton.onclick = function () {
-  if ( mysitenav.getAttribute('data-navstate') ==='open') {
+  if (mysitenav.getAttribute('data-navstate') === 'open') {
     mysitenav.setAttribute('data-navstate', 'closed');
   } else {
-      mysitenav.setAttribute('data-navstate', 'open');
-    }
+    mysitenav.setAttribute('data-navstate', 'open');
   }
+}
 
 // CHANGE ACTIVE STATE FOR ALL SECTIONS WITH INTERSECTION OBSERVER
-    const myobserver = new IntersectionObserver(entries => {
-      entries.forEach(entry => {
-          if (entry.isIntersecting) {
-              entry.target.setAttribute('data-sectionstate', 'active');
-          } else {
-              entry.target.setAttribute('data-sectionstate', 'inactive');
-          }
-      });
+const myobserver = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.setAttribute('data-sectionstate', 'active');
+    } else {
+      entry.target.setAttribute('data-sectionstate', 'inactive');
+    }
   });
-  document.querySelectorAll('.animate-on-scroll').forEach( (section) => {
-      myobserver.observe(section);
-  });
+});
+document.querySelectorAll('.animate-on-scroll').forEach((section) => {
+  myobserver.observe(section);
+});
